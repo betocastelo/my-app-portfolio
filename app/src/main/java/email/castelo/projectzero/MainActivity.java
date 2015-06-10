cb2a3a3daf93de81fb07f010fb789ef177cb6102
Beto Castelo <adalberto.castelo@gmail.com>
2015-06-10 08:25:56 -0400

    Implemented suggestions by code reviewer (thanks again!).

--- app/src/main/java/email/castelo/projectzero/MainActivity.java
+++ app/src/main/java/email/castelo/projectzero/MainActivity.java
@@ -1,6 +1,7 @@
 package email.castelo.projectzero;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,28 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
-
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set listeners for the app buttons in the main view.
-        ArrayList<Button> listOfButtons = new ArrayList<Button>();
-        listOfButtons.add((Button) findViewById(R.id.spotify_streamer_button));
-        listOfButtons.add((Button) findViewById(R.id.scores_app_button));
-        listOfButtons.add((Button) findViewById(R.id.library_app_button));
-        listOfButtons.add((Button) findViewById(R.id.build_it_bigger_button));
-        listOfButtons.add((Button) findViewById(R.id.xyz_reader_button));
-        listOfButtons.add((Button) findViewById(R.id.capstone_button));
-
-        for (Button button:listOfButtons) {
-            button.setOnClickListener(this);
-        }
     }
 
     @Override
@@ -54,35 +40,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View view) {
-        String message = getString(R.string.starting_app_message) + " ";
-        switch (view.getId()) {
-            case R.id.spotify_streamer_button:
-                message += getString(R.string.spotify_streamer);
-                break;
+    public void displayToast(View view) {
+        Button button = (Button) view;
+        String buttonText = (String) button.getText();
 
-            case R.id.scores_app_button:
-                message += getString(R.string.scores_app);
-                break;
+        Context context = getApplicationContext();
+        // open_app would add "Opens the app "
+        CharSequence text = getString(R.string.starting_app_message)
+                + buttonText;
+        int duration = Toast.LENGTH_SHORT;
 
-            case R.id.library_app_button:
-                message += getString(R.string.library_app);
-                break;
-
-            case R.id.build_it_bigger_button:
-                message += getString(R.string.build_it_bigger);
-                break;
-
-            case R.id.xyz_reader_button:
-                message += getString(R.string.xyz_reader);
-                break;
-
-            case R.id.capstone_button:
-                message += getString(R.string.capstone_project);
-                break;
-        }
-
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
